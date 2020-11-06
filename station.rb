9067cd43f9d1344df3b9d22fa94dd45cfb87286d
PapaKramb <guccigang5554@gmail.com>
2020-11-06 20:35:13 +0900

    Update home work

--- station.rb
+++ station.rb
@@ -1,31 +1,28 @@
 class Station
-	attr_reader :name
-	attr_reader :trains
+  attr_reader :name, :trains
 
-	def initialize(name)
-		@name = name
-		@trains = []
-	end
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
 
-	def get_train(train)
-		self.trains << train
-	end
+  def get_train(train)
+    trains << train
+  end
 
-	def show_trains
-		self.trains.each do |train|
-			puts "Список поездов #{train}"
-		end
-	end
+  def show_trains
+    trains.each do |train|
+      puts "Список поездов #{train}"
+    end
+  end
 
-	def type_trains(type)
-		self.trains.each do |train|
-			if train.type == type
-				puts "Тип поезда: #{train}"
-			end
-		end
-	end
+  def type_trains(type)
+    trains.each do |train|
+      puts "Тип поезда: #{train}" if train.type == type
+    end
+  end
 
-	def send_train(train)
-		self.trains.delete(train)
-	end
+  def send_train(train)
+    trains.delete(train)
+  end
 end
--- train.rb
+++ train.rb
@@ -1,12 +1,8 @@
 class Train
-	attr_reader :number
-	attr_reader :type
-	attr_reader :route
-	attr_accessor :speed
-	attr_accessor :count
-	attr_accessor :station
+  attr_reader :number, :type, :route
+  attr_accessor :speed, :count, :station
 
-	def initialize(number, type, count)
+  def initialize(number, type, count)
     @number = number
     @type = type
     @count = count
@@ -14,45 +10,61 @@ class Train
   end
 
   def train_speed(speed)
-  	self.speed += speed 
-  	puts "Текущая скорость: #{speed}"
+    self.speed += speed
+    puts "Текущая скорость: #{speed}"
   end
 
   def stop
-  	self.speed = 0
-  	puts "Текущая скорость: #{speed}"
+    self.speed = 0
+    puts "Текущая скорость: #{speed}"
   end
 
   def add_wagon
-  	if self.speed == 0
-  		self.count += 1
-  	else
-  		puts "Остановитесь чтобы прицепить вагон"
-  	end
+    if self.speed == 0
+      self.count += 1
+    else
+      puts 'Остановитесь чтобы прицепить вагон'
+    end
   end
 
-	def delete_wagon
-  	if self.speed == 0
-  		self.count -= 1
-  	else
-  		puts "Остановитесь чтобы отцепить вагон"
-  	end
-  end 
+  def delete_wagon
+    if self.speed == 0
+      self.count -= 1
+    else
+      puts 'Остановитесь чтобы отцепить вагон'
+    end
+  end
 
   def route=(route)
     @route = route
     self.station = self.route.stations.first
-  end 
+  end
+
+  def move_forward
+    return if @route.nil?
+
+    station.send_train
+    self.station = next_station
+    station.get_train
+  end
+
+  def move_back
+    return if @route.nil?
+
+    station.send_train
+    self.station = previous_station
+    station.get_train
+  end
 
   def next_station
-  	self.route.stations[self.route.stations.index(self.station) + 1]
+    route.stations[route.stations.index(station) + 1]
   end
 
   def previous_station
-  	self.route.stations[self.route.stations.index(self.station) - 1]
+    route.stations[route.stations.index(station) - 1]
   end
 
   def move_next_station
-  	self.station = self.route.stations[self.route.staions.index(self.staion) + 1]
+    self.station = route.stations[route.staions.index(staion) + 1]
   end
 end
