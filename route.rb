749b986b63aabda53e3d79a6c1d65fabf9f4f46c
PapaKramb <guccigang5554@gmail.com>
2020-11-14 23:44:27 +0900

    Lesson6

--- route.rb
+++ route.rb
@@ -4,8 +4,18 @@ class Route
   attr_reader :stations
 
   def initialize(start_station, finish_station)
+    @start_station = start_station
+    @finish_station = finish_station
     @stations = [start_station, finish_station]
+    validate!
     register_instance
+  end
+
+  def valid?
+    validate!
+    true
+  rescue StandardError
+    false
   end
 
   def add_intermediate(station)
@@ -20,7 +30,11 @@ class Route
     @stations.each { |station| puts station.name }
   end
 
-  private
+  protected
 
-  attr_writer :stations
+  def validate!
+    raise 'Первая и конечная станция совпадают' if @start_station == @finish_station
+    raise 'Поезда некуда ехать' if @stations.size < 2
+    raise 'TypeError' unless @stations.all? { |station| station.is_a?(Station) }
+  end
 end
